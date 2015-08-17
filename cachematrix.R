eb8ed4ccb2022ae7e0b7abf26aa278eb2459b9a8
ChinoMing <ming4mifos@gmail.com>
2015-08-17 21:10:31 +0100

    Revert "Test comment"
    This reverts commit e0f30f6b260fe14073bf64fb267ea0edc9fcf0c8.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,6 @@
 ## makeCacheMatrix: This function creates a special "matrix" object that can 
 ## cache its inverse.
 
-# Hello
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
